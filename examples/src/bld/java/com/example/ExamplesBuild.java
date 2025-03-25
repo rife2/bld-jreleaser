4a5da76a62dedbfe593f748c902df7f171d20495
Erik C. Thauvin <erik@thauvin.net>
2025-03-25 09:54:01 -0700

    refactor(examples): Add extensions debug logging

--- examples/src/bld/java/com/example/ExamplesBuild.java
+++ examples/src/bld/java/com/example/ExamplesBuild.java
@@ -5,6 +5,9 @@ import rife.bld.Project;
 import rife.bld.extension.JReleaserInitOperation;
 
 import java.util.List;
+import java.util.logging.ConsoleHandler;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 import static rife.bld.dependencies.Repository.*;
 import static rife.bld.dependencies.Scope.*;
@@ -29,6 +32,16 @@ public class ExamplesBuild extends Project {
     }
 
     public static void main(String[] args) {
+        // Enable detailed logging for the extensions
+        var level = Level.ALL;
+        var logger = Logger.getLogger("rife.bld.extension");
+        var consoleHandler = new ConsoleHandler();
+
+        consoleHandler.setLevel(level);
+        logger.addHandler(consoleHandler);
+        logger.setLevel(level);
+        logger.setUseParentHandlers(false);
+
         new ExamplesBuild().start(args);
     }
 
