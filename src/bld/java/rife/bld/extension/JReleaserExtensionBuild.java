342812af7fd356657aec948e56b9f1132d70173d
Erik C. Thauvin <erik@thauvin.net>
2025-07-11 23:21:59 -0700

    build(deps): Bump bld to version 2.3.0

--- src/bld/java/rife/bld/extension/JReleaserExtensionBuild.java
+++ src/bld/java/rife/bld/extension/JReleaserExtensionBuild.java
@@ -42,7 +42,7 @@ public class JReleaserExtensionBuild extends Project {
         repositories = List.of(MAVEN_LOCAL, MAVEN_CENTRAL, RIFE2_RELEASES, RIFE2_SNAPSHOTS);
 
         scope(compile)
-                .include(dependency("com.uwyn.rife2", "bld", version(2, 2, 1)));
+                .include(dependency("com.uwyn.rife2", "bld", version(2, 3, 0)));
         scope(test)
                 .include(dependency("org.junit.jupiter", "junit-jupiter", version(5, 12, 2)))
                 .include(dependency("org.junit.platform", "junit-platform-console-standalone", version(1, 12, 2)));
