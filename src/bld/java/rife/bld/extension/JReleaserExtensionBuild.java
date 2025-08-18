14040ec799f40364f66dde6eba1c05a349bafd3a
Erik C. Thauvin <erik@thauvin.net>
2025-08-17 17:45:55 -0700

    bld(deps): Add Testing Helpers as a dependency

--- src/bld/java/rife/bld/extension/JReleaserExtensionBuild.java
+++ src/bld/java/rife/bld/extension/JReleaserExtensionBuild.java
@@ -44,9 +44,14 @@ public class JReleaserExtensionBuild extends Project {
         scope(compile)
                 .include(dependency("com.uwyn.rife2", "bld", version(2, 3, 0)));
         scope(test)
-                .include(dependency("org.jreleaser", "jreleaser", version(1,19,0)))
-                .include(dependency("org.junit.jupiter", "junit-jupiter", version(5, 13, 4)))
-                .include(dependency("org.junit.platform", "junit-platform-console-standalone", version(1, 13, 4)));
+                .include(dependency("com.uwyn.rife2", "bld-extensions-testing-helpers",
+                        version(0, 9, 0, "SNAPSHOT")))
+                .include(dependency("org.jreleaser", "jreleaser",
+                        version(1,19,0)))
+                .include(dependency("org.junit.jupiter", "junit-jupiter",
+                        version(5, 13, 4)))
+                .include(dependency("org.junit.platform", "junit-platform-console-standalone",
+                        version(1, 13, 4)));
 
         javadocOperation()
                 .javadocOptions()
