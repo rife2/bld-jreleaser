257d761f04b9b29bc2462938f258c8dfb9010b27
Erik C. Thauvin <erik@thauvin.net>
2025-11-14 10:05:19 -0800

    build(deps): Bump Testing Helpers to version 0.9.4

--- src/bld/java/rife/bld/extension/JReleaserExtensionBuild.java
+++ src/bld/java/rife/bld/extension/JReleaserExtensionBuild.java
@@ -48,7 +48,7 @@ public class JReleaserExtensionBuild extends Project {
                 .include(dependency("com.uwyn.rife2", "bld", version(2, 3, 0)));
         scope(test)
                 .include(dependency("com.uwyn.rife2", "bld-extensions-testing-helpers",
-                        version(0, 9, 3, "SNAPSHOT")))
+                        version(0, 9, 4)))
                 .include(dependency("org.jreleaser", "jreleaser",
                         version(1, 20, 0)))
                 .include(dependency("org.junit.jupiter", "junit-jupiter", junit))
