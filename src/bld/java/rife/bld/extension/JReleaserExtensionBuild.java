f50de237682e99d9ac3124b99274d5904c139f09
Erik C. Thauvin <erik@thauvin.net>
2026-01-07 18:00:33 -0800

    build(deps): Bump JUnit to version 6.0.2

--- src/bld/java/rife/bld/extension/JReleaserExtensionBuild.java
+++ src/bld/java/rife/bld/extension/JReleaserExtensionBuild.java
@@ -43,7 +43,7 @@ public class JReleaserExtensionBuild extends Project {
 
         repositories = List.of(MAVEN_CENTRAL, RIFE2_RELEASES);
 
-        var junit = version(6, 0, 1);
+        var junit = version(6, 0, 2);
         scope(compile)
                 .include(dependency("com.uwyn.rife2", "bld", version(2, 3, 0)));
         scope(test)
